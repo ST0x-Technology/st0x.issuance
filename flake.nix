4a64e9a62e80e6cea515c0b245b903da7fb15637
rouzwelt <rouzweltt@gmail.com>
2026-06-24 23:58:23 +0000

    devshell abi envs

--- flake.nix
+++ flake.nix
@@ -371,8 +371,8 @@
         # what builds them and runs the TypeScript exporter in CI.
         checks = issuanceBuilds;
 
-        devShell =
-          pkgs.mkShell {
+        devShell = pkgs.mkShell (
+          {
             # inherit (rainix.devShells.${system}.default) shellHook;
             inherit (rainix.devShells.${system}.default) nativeBuildInputs;
             shellHook = ''
@@ -402,7 +402,8 @@
 
             DATABASE_URL = "sqlite:./issuance.db";
           }
-          // abiEnv;
+          // abiEnv
+        );
       }
     );
 }
