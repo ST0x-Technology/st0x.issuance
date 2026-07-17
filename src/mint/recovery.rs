847fc8dd4b5379092c7c516a7503bdfa98766e0f
0xgleb <39841057+0xgleb@users.noreply.github.com>
2026-07-17 20:23:52 -0300

    fix: repair post-restack test integration

--- src/mint/recovery.rs
+++ src/mint/recovery.rs
@@ -1912,6 +1912,7 @@ mod tests {
                 harness.pool.clone(),
                 (),
             ));
+            let vault_services = network_vault_services(harness.vault.clone());
 
             let TestHarness { pool, apalis_pool, mint_store, vault, .. } =
                 harness;
