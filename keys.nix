0373f2dd2aef11d5b970362f821cebc9da540834
JuaniRios <juani.rios.99@gmail.com>
2026-08-13 15:00:44 -0300

    chore: revoke Alex and Jakub server access

--- keys.nix
+++ keys.nix
@@ -4,8 +4,6 @@ rec {
     juan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHHNeV3nfiJS0QE2JoW3d0dRw1j6OVKl7rXor24XHvsd";
     # purpose: op sec auditing and manual cli while fixing gaps in automation
     alastair = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJArH3PA+bFIon0JkCVQGs9aWr45lnVjiiTLLO9BPItn";
-    jakub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM8D2RLBbEfB0GZKhT7tlT43pkKbrMnOR7cZfXoG9NbH";
-    alex = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA9MytndYpB/zpdh4kCXE0KF+NoLrpe+ij+zctM3xUCf";
 
     # purpose: deployments from github actions
     ci = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkrujoBNXILB8Ypv90puKKhmKq161gitlvUFM5n0c+d";
@@ -34,8 +32,6 @@ rec {
       ssh = [
         juan
         alastair
-        jakub
-        alex
         st0x-op
         ci
       ];
@@ -43,8 +39,6 @@ rec {
         st0x-op
         juan
         alastair
-        jakub
-        alex
         host-prod
       ];
     };
@@ -53,8 +47,6 @@ rec {
       ssh = [
         juan
         alastair
-        jakub
-        alex
         st0x-op
         ci
       ];
@@ -62,8 +54,6 @@ rec {
         st0x-op
         juan
         alastair
-        jakub
-        alex
         host-staging
       ];
     };
